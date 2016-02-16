ff8986abf5206294c0afdd560128ac283c95daff
Eivind Siqveland Larsen <eivind.larsen@bekk.no>
2016-02-16 09:41:00 +0100

    cleanup request routine if Client closes connection

--- main.go
+++ main.go
@@ -19,6 +19,99 @@ const port = ":7999"
 func main() {
 	c := xhandler.Chain{}
 
+	setupHandlerChain(&c)
+
+	mux := xmux.New()
+
+	mux.GET("/gamble", xhandler.HandlerFuncC(shittyHandler))
+
+	log.Printf("Listening on http://localhost%v/gamble", port)
+	http.ListenAndServe(port, c.Handler(mux))
+
+}
+
+func shittyHandler(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
+	i := rand.Intn(1000)
+
+	if i < 5 {
+		eternalRequest(ctx, rw, r)
+		return
+	}
+	if i < 30 {
+		longRequest(ctx, rw, r)
+		return
+
+	}
+	if i < 50 {
+		EmptyBody(ctx, rw, r)
+		return
+	}
+	normalResponse(ctx, rw, r)
+}
+
+func normalResponse(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
+	l := xlog.FromContext(ctx)
+
+	millis := time.Duration(rand.Intn(150)+50) * time.Millisecond
+
+	l.Infof("normal response %vms", millis)
+
+	select {
+	case <-time.After(millis):
+		rw.Write(response)
+		return
+	case <-ctx.Done():
+		l.Infof("Connection dead")
+		return
+	}
+}
+
+func longRequest(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
+	l := xlog.FromContext(ctx)
+
+	minutes := time.Duration(rand.Intn(60)+30) * time.Minute
+	l.Infof("longRequest %v", minutes)
+
+	select {
+	case <-time.After(minutes):
+		rw.Write(response)
+		return
+	case <-ctx.Done():
+		// make sure we cleanup if the client hangs up
+		l.Infof("Connection dead")
+		return
+	}
+}
+
+func eternalRequest(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
+	l := xlog.FromContext(ctx)
+
+	hours := time.Duration(rand.Intn(24)) * time.Hour
+	l.Infof("eternalRequest %v", hours)
+
+	select {
+	case <-time.After(hours):
+		rw.Write(response)
+		return
+	case <-ctx.Done():
+		l.Infof("Connection dead")
+		return
+	}
+}
+
+// EmptyBody writes a http.StatusServiceUnavailable to indicate a temporary service failure
+// This response should make the client retry.
+func EmptyBody(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
+	l := xlog.FromContext(ctx)
+
+	l.Info("emptyBody")
+
+	rw.WriteHeader(http.StatusServiceUnavailable)
+	rw.Write(nil)
+}
+
+// setupHandlerChain does plumbing for logging and such.
+func setupHandlerChain(c *xhandler.Chain) {
 	host, _ := os.Hostname()
 	conf := xlog.Config{
 		// Log info level and higher
@@ -50,65 +143,4 @@ func main() {
 	c.UseC(xlog.UserAgentHandler("user_agent"))
 	c.UseC(xlog.RefererHandler("referer"))
 	c.UseC(xlog.RequestIDHandler("req_id", "Request-Id"))
-
-	mux := xmux.New()
-
-	mux.GET("/gamble", xhandler.HandlerFuncC(shittyHandler))
-
-	log.Printf("Listening on http://localhost%v/gamble", port)
-	http.ListenAndServe(port, c.Handler(mux))
-
-}
-
-func shittyHandler(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
-	i := rand.Intn(1000)
-
-	if i < 5 {
-		eternalRequest(ctx, rw, r)
-		return
-	}
-	if i < 30 {
-		longRequest(ctx, rw, r)
-		return
-
-	}
-	if i < 50 {
-		emptyBody(ctx, rw, r)
-		return
-	}
-	normalResponse(ctx, rw, r)
-}
-
-func normalResponse(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
-	l := xlog.FromContext(ctx)
-	l.Info("normal response")
-	rw.Write(response)
-}
-
-func longRequest(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
-	l := xlog.FromContext(ctx)
-
-	minutes := time.Duration(rand.Intn(60) + 30) * time.Minute
-	l.Infof("longRequest %v", minutes)
-
-	time.Sleep(minutes)
-	rw.Write(response)
-}
-
-func eternalRequest(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
-	l := xlog.FromContext(ctx)
-
-	hours := time.Duration(rand.Intn(24)) * time.Hour
-	l.Infof("eternalRequest %v", hours)
-
-	time.Sleep(hours)
-	rw.Write(response)
-}
-
-func emptyBody(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
-	l := xlog.FromContext(ctx)
-
-	l.Info("emptyBody")
-
-	rw.Write(nil)
 }
